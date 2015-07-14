91591cb3af3391cb31995d17b49a61d4c1558666
elvece <lvcifferello@gmail.com>
2015-07-13 22:39:20 -0600

    added solution to birth date problem

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,17 @@
-var phone = prompt("Enter your phone number in the following format: XXX-XXX-XXXX");
+//phone number
+// var phone = prompt("Enter your phone number in the following format: XXX-XXX-XXXX");
+//
+// if (phone.charAt(3) === "-" && phone.charAt(7) === "-") {
+//   alert("Thank you.");
+// } else {
+//   alert("You did not enter your phone number correctly. Please try again.");
+// }
 
-if (phone.charAt(3) === "-" && phone.charAt(7) === "-") {
-  console.log("Thank you.");
+//birth date
+var birth = prompt("Enter your birth date in the following format: MM/DD/YY");
+
+if (birth.charAt(2) === "/" && birth.charAt(5) === "/") {
+  alert("Thank you.");
 } else {
-  console.log("You did not enter your phone number correctly. Please try again.");
+  alert("You did not enter your birth date correctly. Please try again.");
 }
