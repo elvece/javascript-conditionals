92e67c561c6ee3566743c190e95afc0c5332ddb9
elvece <lvcifferello@gmail.com>
2015-07-13 22:42:00 -0600

    noticed importance of input length when started postal code

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,7 @@
 //phone number
 // var phone = prompt("Enter your phone number in the following format: XXX-XXX-XXXX");
 //
-// if (phone.charAt(3) === "-" && phone.charAt(7) === "-") {
+// if (phone.charAt(3) === "-" && phone.charAt(7) === "-" && phone.length === 12) {
 //   alert("Thank you.");
 // } else {
 //   alert("You did not enter your phone number correctly. Please try again.");
@@ -10,8 +10,10 @@
 //birth date
 var birth = prompt("Enter your birth date in the following format: MM/DD/YY");
 
-if (birth.charAt(2) === "/" && birth.charAt(5) === "/") {
+if (birth.charAt(2) === "/" && birth.charAt(5) === "/" && birth.length === 8) {
   alert("Thank you.");
 } else {
   alert("You did not enter your birth date correctly. Please try again.");
 }
+
+//postal code
